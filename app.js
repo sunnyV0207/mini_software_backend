e2c90d7c1bbc8cd38ae66b3e451c80b26955514f
Sunny Verma <sunnyvermaverma2005@gmail.com>
2025-11-27 17:48:56 +0530

    feat: Update CORS configuration to allow multiple origins

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://mini-software-frontend.vercel.app/','http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
